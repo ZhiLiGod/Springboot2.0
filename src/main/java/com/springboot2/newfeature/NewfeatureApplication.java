feff2258e88406c9776afb5addd21dc00e58190b
Zhi Li <zhi.li@invoicefair.com>
2020-02-23 10:28:27 +0000

    first commit

---
+++ src/main/java/com/springboot2/newfeature/NewfeatureApplication.java
@@ -0,0 +1,13 @@
+package com.springboot2.newfeature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewfeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewfeatureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot2/newfeature/NewfeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot2.newfeature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewfeatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

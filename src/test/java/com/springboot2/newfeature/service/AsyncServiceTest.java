696760973354058b9cc826efdd9e62462ea5fcb1
Zhi Li <zhi.li@invoicefair.com>
2020-03-08 12:40:41 +0000

    test_2

--- src/test/java/com/springboot2/newfeature/service/AsyncServiceTest.java
+++ src/test/java/com/springboot2/newfeature/service/AsyncServiceTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.concurrent.Future;
+
 @Slf4j
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -19,6 +21,14 @@ public class AsyncServiceTest {
   public void test_1() throws Exception {
     asyncService.asynvProcess();
     log.info("coming in test_1");
+  }
+
+  @Test
+  public void test_2() throws Exception {
+    long start = System.currentTimeMillis();
+    Future<Integer> result = asyncService.asyncProcessReturn();
+    log.info("get async task value: {}", result.get());
+    log.info("Time elapse: {}", System.currentTimeMillis() - start);
   }
 
 }
